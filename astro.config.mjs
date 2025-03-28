9a10a3d45552d3c726b9b6aee8ec65af2d760024
Akash A <akashanil084@gmail.com>
2025-03-28 23:23:31 +0530

    Revert "feat: add deploy.yml"
    This reverts commit 054f1c943dc355df7bb5be1b00cb9cadb9945070.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  site: 'https://ecoholic84.github.io',
-  base: '/portfolio',
   vite: {
     optimizeDeps: {
       include: ["zwitch"],
